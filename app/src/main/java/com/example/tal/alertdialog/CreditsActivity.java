d3f1a6540ffe91eac7cedc006c3c507a1db4eed8
Tal <ty4273@bs.amalnet.k12.il>
2018-11-08 21:38:22 +0200

    Final commit

--- app/src/main/java/com/example/tal/alertdialog/CreditsActivity.java
+++ app/src/main/java/com/example/tal/alertdialog/CreditsActivity.java
@@ -2,6 +2,7 @@ package com.example.tal.alertdialog;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class CreditsActivity extends AppCompatActivity {
 
@@ -9,5 +10,17 @@ public class CreditsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credits);
+        setTitle("Credits");
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home)
+            this.finish();
+        return super.onOptionsItemSelected(item);
     }
 }
